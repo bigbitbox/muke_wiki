f3c34b9fbf860b875c68eea51b88fc8af9fc2089
rui <ungatz@hotmail.com>
2022-11-08 15:58:00 +0800

    2-1 封装请求响应处理类  包括使用工具类进行响应处理

--- wiki/src/main/java/com/rrzui/wiki/controller/EbookController.java
+++ wiki/src/main/java/com/rrzui/wiki/controller/EbookController.java
@@ -1,7 +1,12 @@
 package com.rrzui.wiki.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.rrzui.wiki.entity.Ebook;
+import com.rrzui.wiki.req.EbookReq;
+import com.rrzui.wiki.resp.CommonResp;
+import com.rrzui.wiki.resp.EbookResp;
 import com.rrzui.wiki.service.EbookService;
+import com.rrzui.wiki.utils.CopyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +19,45 @@ public class EbookController {
     @Autowired
     private EbookService ebookService;
 
+//    @GetMapping("/getEbooks")
+//    public List<Ebook> getEbooks(){
+//        List<Ebook> list = ebookService.list();
+//        for (Ebook ebook : list) {
+//            System.out.println(ebook.toString());
+//        }
+//        return list;
+//    }
+
     @GetMapping("/getEbooks")
-    public List<Ebook> getEbooks(){
+    public CommonResp getEbooks(){
         List<Ebook> list = ebookService.list();
         for (Ebook ebook : list) {
             System.out.println(ebook.toString());
         }
-        return list;
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        resp.setContent(list);
+        return resp;
+    }
+
+    @GetMapping("/getEbookByName")
+    public CommonResp getEbookByName(EbookReq ebookReq){
+        QueryWrapper<Ebook> wrapper = new QueryWrapper<>();
+        wrapper.like("name",ebookReq.getName());
+        List<Ebook> list = ebookService.list(wrapper);
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        resp.setContent(list);
+        return resp;
+    }
+
+    @GetMapping("/getEbookByEbookReq")
+    public CommonResp getEbookByEbookReq(EbookReq ebookReq){
+        QueryWrapper<Ebook> wrapper = new QueryWrapper<>();
+        wrapper.like("name",ebookReq.getName());
+        List<Ebook> list = ebookService.list(wrapper);
+
+        List<EbookResp> ebookResps = CopyUtil.copyList(list, EbookResp.class);
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        resp.setContent(ebookResps);
+        return resp;
     }
 }
---
+++ wiki/src/main/java/com/rrzui/wiki/req/EbookReq.java
@@ -0,0 +1,9 @@
+package com.rrzui.wiki.req;
+
+import lombok.Data;
+
+@Data
+public class EbookReq {
+    private String name;
+    private int id;
+}
