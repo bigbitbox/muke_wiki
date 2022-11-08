2afd43f6ea53776555d6fde7eeff826c15060c20
rui <ungatz@hotmail.com>
2022-11-08 09:41:48 +0800

    1-3 修改启动类

--- wiki/src/main/java/com/rrzui/wiki/WikiApplication.java
+++ wiki/src/main/java/com/rrzui/wiki/WikiApplication.java
@@ -2,12 +2,24 @@ package com.rrzui.wiki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+@ComponentScan("com.rrzui")
 @SpringBootApplication
-public class WikiApplication {
+public class WikiApplication
+{
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 
-    public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+    public static void main( String[] args )
+    {
+        //SpringApplication.run(WikiApplication.class,args);
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
+
     }
-
 }
