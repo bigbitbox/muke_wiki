7c9f325367dba1b7dbf82327560f5b464099ec82
rui <ungatz@hotmail.com>
2022-11-09 17:00:29 +0800

    3-9 修改后端 做是否为空的判断

--- wiki/src/main/java/com/rrzui/wiki/controller/EbookController.java
+++ wiki/src/main/java/com/rrzui/wiki/controller/EbookController.java
@@ -8,6 +8,7 @@ import com.rrzui.wiki.resp.EbookResp;
 import com.rrzui.wiki.service.EbookService;
 import com.rrzui.wiki.utils.CopyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -51,9 +52,17 @@ public class EbookController {
 
     @GetMapping("/getEbookByEbookReq")
     public CommonResp getEbookByEbookReq(EbookReq ebookReq){
-        QueryWrapper<Ebook> wrapper = new QueryWrapper<>();
-        wrapper.like("name",ebookReq.getName());
-        List<Ebook> list = ebookService.list(wrapper);
+
+        List<Ebook> list;
+        if (!ObjectUtils.isEmpty(ebookReq.getName())) {
+            QueryWrapper<Ebook> wrapper = new QueryWrapper<>();
+            wrapper.like("name",ebookReq.getName());
+            list = ebookService.list(wrapper);
+        }else {
+            list = ebookService.list();
+        }
+
+
 
         List<EbookResp> ebookResps = CopyUtil.copyList(list, EbookResp.class);
         CommonResp<List<EbookResp>> resp = new CommonResp<>();
