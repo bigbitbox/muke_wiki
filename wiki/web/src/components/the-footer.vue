9729e4dd6ccfd6fe7d5c3ab4073dece6d389cf64
rui <ungatz@hotmail.com>
2022-11-09 14:25:38 +0800

    3-2 head 和 footer 组件分离

---
+++ wiki/web/src/components/the-footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <a-layout-footer style="text-align: center">
+      wiki电子书网站te
+    </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomeView',
+});
+
+</script>
+
+<style scoped>
+
+</style>
