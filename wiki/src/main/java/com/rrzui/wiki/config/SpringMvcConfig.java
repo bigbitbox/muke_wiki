a1e63434b87627a36c643fa5b368461ffdccf67d
rui <ungatz@hotmail.com>
2022-11-10 14:32:44 +0800

    3-13 实现日志的部署， 使用AOP管理日志

---
+++ wiki/src/main/java/com/rrzui/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,22 @@
+//package com.rrzui.wiki.config;
+//
+//
+//import com.rrzui.wiki.interceptor.LogInterceptor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Autowired
+//    LogInterceptor logInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**");
+//    }
+//
+//
+//}
