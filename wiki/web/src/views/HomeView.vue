d07350b507fc11b61bbc67e3c3335aec2b8dec8f
rui <ungatz@hotmail.com>
2022-11-14 14:10:15 +0800

    5 - 3 完成首页的开发

--- wiki/web/src/views/HomeView.vue
+++ wiki/web/src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <a-layout>
       <a-layout-sider width="200" style="background: #fff">
-        <a-menu mode="inline" :style="{ height: '100%', borderRight: 0 }">
+        <a-menu mode="inline" :style="{ height: '100%', borderRight: 0 }" @click="handleClick">
           <a-menu-item key="welcome">
             <MailOutlined />
             <span>欢迎</span>
@@ -15,44 +15,7 @@
               <MailOutlined /><span>{{ child.name }}</span>
             </a-menu-item>
           </a-sub-menu>
-
-          <a-sub-menu key="sub1">
-            <template #title>
-              <span>
-                <user-outlined />
-                subnav 1
-              </span>
-            </template>
-            <a-menu-item key="1">option1</a-menu-item>
-            <a-menu-item key="2">option2</a-menu-item>
-            <a-menu-item key="3">option3</a-menu-item>
-            <a-menu-item key="4">option4</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub2">
-            <template #title>
-              <span>
-                <laptop-outlined />
-                subnav 2
-              </span>
-            </template>
-            <a-menu-item key="5">option5</a-menu-item>
-            <a-menu-item key="6">option6</a-menu-item>
-            <a-menu-item key="7">option7</a-menu-item>
-            <a-menu-item key="8">option8</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub3">
-            <template #title>
-              <span>
-                <notification-outlined />
-                subnav 3
-              </span>
-            </template>
-            <a-menu-item key="9">option9</a-menu-item>
-            <a-menu-item key="10">option10</a-menu-item>
-            <a-menu-item key="11">option11</a-menu-item>
-            <a-menu-item key="12">option12</a-menu-item>
-          </a-sub-menu>
-        </a-menu>
+        </a-menu >
       </a-layout-sider>
       <a-layout-content
         :style="{
@@ -62,12 +25,12 @@
           minHeight: '280px',
         }"
       >
-        <!-- <pre>{{ ebooks }}</pre>
-        <br />
-        <br />
-        <pre>{{ ebooks2 }}</pre> -->
+        <div class="welcome" v-show="isShowWelcom">
+          <h1>欢迎使用wiki知识库</h1>
+        </div>
 
         <a-list
+          v-show="!isShowWelcom"
           item-layout="vertical"
           size="large"
           :grid="{ gutter: 20, column: 3 }"
@@ -135,8 +98,19 @@ export default defineComponent({
       });
     };
 
-    const handleClick = () => {
-      console.log("menu click");
+    const isShowWelcom = ref(true);
+    const handleClick = (value:any) => {
+      // console.log("menu click",value);
+      // if (value.key === 'welcome'){
+      //   isShowWelcom.value = true;
+      // } else {
+      //   isShowWelcom.value = false;
+      // }
+
+      /**
+       * 代码简化
+       */
+        isShowWelcom.value = value.key === 'welcome';
     };
 
     onMounted(() => {
@@ -159,6 +133,7 @@ export default defineComponent({
     return {
       level1,
       handleClick,
+      isShowWelcom,
 
       ebooks,
       ebooks2: toRef(ebooks2, "books"),
